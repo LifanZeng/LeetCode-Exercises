3ed1fd76bb91cbe8250944d75b0f173e6dd23e63
Lifan Zeng <lifan.zeng@sjsu.edu>
2022-07-20 09:57:55 -0700

    7/20/2022, Populating next right pointers for each node in BinaryTree

---
+++ 20220720_0604_116R_populatingNextRightPointersInEachNode/src/Tester.java
@@ -0,0 +1,45 @@
+public class Tester {
+    public static void main(String[] args){
+        Node root = new Node(1, new Node(2, new Node(4), new Node(5), null),
+                new Node(3, new Node(6), new Node(7), null), null);
+
+        Solution solution = new Solution();
+        solution.connect(root);
+
+        Node tmp = root;
+        while(true){
+            System.out.print(tmp.val + ", ");
+            if(tmp.next != null) {
+                tmp = tmp.next;
+            }
+            else{
+                System.out.print("#" + ", ");
+                break;
+            }
+        }
+
+        tmp = root.left;
+        while(true){
+            System.out.print(tmp.val + ", ");
+            if(tmp.next != null) {
+                tmp = tmp.next;
+            }
+            else{
+                System.out.print("#" + ", ");
+                break;
+            }
+        }
+
+        tmp = root.left.left;
+        while(true){
+            System.out.print(tmp.val + ", ");
+            if(tmp.next != null) {
+                tmp = tmp.next;
+            }
+            else{
+                System.out.print("#" + ", ");
+                break;
+            }
+        }
+    }
+}
