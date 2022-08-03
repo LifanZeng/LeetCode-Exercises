80beac505f792251d3cc4b3f75cb795377b09bf0
Lifan Zeng <lifan.zeng@sjsu.edu>
2022-08-02 23:17:14 -0700

    8/2/2022, Convert BST to greater tree.

---
+++ 20220802_0623_538-1038R_convertBSTtoGreaterTree/src/Solution.java
@@ -0,0 +1,56 @@
+import java.util.LinkedList;
+import java.util.Queue;
+
+public class Solution {
+    public TreeNode convertBST(TreeNode root) {
+        traverse(root);
+        return root;
+    }
+
+    private int sum=0;
+    private void traverse(TreeNode root){
+        if(root==null)
+            return;
+
+        traverse(root.right);
+        sum = sum + root.val;
+        root.val = sum;
+        traverse(root.left);
+    }
+
+
+
+    //preOrder Traversal
+    public void preOrder(TreeNode node){
+        if(node == null){
+            return;
+        }
+        System.out.print(node.val + " ");
+        preOrder(node.left);
+        preOrder(node.right);
+    }
+    //inOrder Traversal
+    public void inOrder(TreeNode node){
+        if(node == null){
+            return;
+        }
+        inOrder(node.left);
+        System.out.print(node.val + " ");
+        inOrder(node.right);
+    }
+    //levelOrder Traversal
+    public void levelOrder(TreeNode node){
+        Queue<TreeNode> queue = new LinkedList<>();
+        queue.add(node);
+        while(!queue.isEmpty()){
+            TreeNode presentNode = queue.remove();
+            System.out.print(presentNode.val + " ");
+            if(presentNode.left != null){
+                queue.add(presentNode.left);
+            }
+            if(presentNode.right != null){
+                queue.add(presentNode.right);
+            }
+        }
+    }
+}
---
+++ 20220802_0623_538-1038R_convertBSTtoGreaterTree/src/Tester.java
@@ -0,0 +1,14 @@
+public class Tester {
+    public static void main(String[] args){
+        TreeNode root = new TreeNode(7, new TreeNode(5, new TreeNode(3, new TreeNode(2), new TreeNode(4)), new TreeNode(6)),
+                new TreeNode(9, new TreeNode(8), new TreeNode(10)));
+
+        Solution solution = new Solution();
+        solution.levelOrder(root);
+        System.out.println();
+
+        solution.convertBST(root);
+        solution.levelOrder(root);
+        System.out.println();
+    }
+}
