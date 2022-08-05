c7af4995cf79fcd3a9e80d0d1f1dcbea3aad4b1a
Lifan Zeng <lifan.zeng@sjsu.edu>
2022-08-04 23:26:03 -0700

    8/4/2022, validate BST.

---
+++ 20220804_0624_98R_validateBinarySearchTree/src/Tester.java
@@ -0,0 +1,15 @@
+public class Tester {
+    public static void main(String[] args){
+        TreeNode root = new TreeNode(70, new TreeNode(50, new TreeNode(30, new TreeNode(20), new TreeNode(40)), new TreeNode(60)),
+                new TreeNode(66, new TreeNode(80), new TreeNode(100)));
+
+        Solution solution = new Solution();
+        solution.levelOrder(root);
+        System.out.println();
+        solution.inOrder(root);
+        System.out.println();
+
+        System.out.println(solution.isValidBST(root));
+
+    }
+}
