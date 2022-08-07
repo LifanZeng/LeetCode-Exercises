71191ebd0368083b5db86252e3f467738c282e7d
Lifan Zeng <lifan.zeng@sjsu.edu>
2022-08-07 06:53:14 -0700

    8/7/2022, Graph basic operations.

---
+++ 20220807_000_graphAdjecencyMatrix/src/Tester.java
@@ -0,0 +1,22 @@
+import java.util.ArrayList;
+
+public class Tester {
+    public static void main(String[] args){
+        ArrayList<GraphNode> nodeList = new ArrayList<GraphNode>();
+        nodeList.add(new GraphNode("A", 0));
+        nodeList.add(new GraphNode("B", 1));
+        nodeList.add(new GraphNode("C", 2));
+        nodeList.add(new GraphNode("D", 3));
+        nodeList.add(new GraphNode("E", 4));
+
+        Graph g = new Graph(nodeList);
+        g.addUndirectedEdge(0, 1);  //A->B, B->A
+        g.addUndirectedEdge(0, 2);  //A->C..
+        g.addUndirectedEdge(0, 3);  //A->D..
+        g.addUndirectedEdge(1, 4);  //B->E..
+        g.addUndirectedEdge(2, 3);  //C->D..
+        g.addUndirectedEdge(3, 4);  //D->E
+
+        System.out.print(g.toString());
+    }
+}
